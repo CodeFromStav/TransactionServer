dc683748c8197775fb4b1d0c7807f852a9801e80
Stavros <stav.triantis@gmail.com>
2020-03-09 11:11:34 -0700

    more comments

--- Account.java
+++ Account.java
@@ -2,5 +2,21 @@
 /*
 number
 balance
+This allows, e.g. for reading an account balance and,
+depending on the value read, writing a different balance back.
 --Have array of accounts, where arrayIndex is the account number
+
+-these two operations can be viewed as a withdrawal or a deposit,
+depending on whether the original balance was larger or smaller
+than the resulting one.
+
+-Only write whole number values for money
+
+an arbitrarily chosen dollar amount is withdrawn
+from an arbitrarily chosen account and deposited
+onto another, arbitrarily chosen account.
+
+--always stay strictly at the low level of elementary 
+read/write operations, instead of  implementing higher 
+level constructs like withdrawal and deposit.
 */
--- TransactionServer.java
+++ TransactionServer.java
@@ -1,6 +1,19 @@
 
 
 /*abstract
+Your transaction server will manage X number
+of data objects that can be thought of as banking
+accounts; each account is holding a certain amount of money.
+The number of accounts is configurable.
+
+-On the other hand, your transactional system,
+ including the server proxy object on the client
+  side, is absolutely unaware of the assumption of 
+  such a predefined sequence of activities, i.e. it 
+  can handle any activities in any order whatsoever, 
+  without any limitation or underlying assumption.
+
+
 
 //Server loop always running in this class
 run()
