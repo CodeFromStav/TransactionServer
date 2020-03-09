a3fa18304d0a047d030525ace91d84d105285bff
AlexBent564 <56088259+AlexBent564@users.noreply.github.com>
2020-03-09 10:55:40 -0700

    Add files via upload

---
+++ TClient.java
@@ -0,0 +1,17 @@
+
+// Transaction Client class
+public class TCLient
+{
+  public openTransaction(){
+
+  }
+  public closeTransaction(){
+
+  }
+  public read(){
+
+  }
+  public write(){
+    
+  }
+}
