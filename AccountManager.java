17a2fd6968d8bfebd0ceb90306d13ae6c5ae014d
AlexBent564 <56088259+AlexBent564@users.noreply.github.com>
2020-03-11 16:06:36 -0700

    yoyo

--- AccountManager.java
+++ AccountManager.java
@@ -11,35 +11,49 @@ write()
 
 public class AccountManager
 {
-    // Constructor
-    public AccountManager()
+  // list of the accounts in the server
+  ArrayList<Account> accounts = new ArrayList<Account>;
+  // Constructor
+  public AccountManager()
+  {
+
+  }
+
+  // Read operation
+    // might not need this?
+  public int read( /* accountnumber, transaction, */)
+  {
+
+  }
+
+  // Write operation
+  public int write( int accountNumber, Transaction transaction, float balance)
+  {
+    // this should add and subtract a balance, not set.
+    Account account = getAccount(accountNumber);
+    TransactionServer.LockManager.Lock( accountnumber, transaction, WRITE_LOCK );
+    account.setBalance(balance);
+    return(account.getAccountBalance());
+  }
+
+
+  // method that loops through list and returns the account if found
+  public Account getAccount( int accountNumber )
+  {
+    for( int i = 0; i < accounts.size(); i++ )
     {
-
+      if( accounts.get(i).getAccountNum() == accountNumber )
+      {
+        return accounts.get(i);
+      }
     }
 
-    // Read operation
-    public int read( /* accountnumber, transaction, */)
-    {  
+    System.out.println("Account does not exist");
+    return null;
+  }
 
-    }
+  public void giveAccountNumber()
+  {
 
-    // Write operation
-    public int write( int accountnumber, Transaction transaction, float balance)
-    {
-        //get account
-        //lock 
-        //set balance
-        //return balance
-    }
-        
-
-    public void getAccount()
-    {
-
-    }
-
-    public void giveAccountNumber()
-    {
-
-    }
+  }
 }
--- LockTypes.java
+++ LockTypes.java
@@ -1,8 +1,8 @@
 
 
 // LockTypes class
-interface LockTypes
-{   
-    
-
+public class LockTypes
+{
+  public int final WRITE_LOCK = 105;
+  public int final READ_LOCK = 106;
 }
