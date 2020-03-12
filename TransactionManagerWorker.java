0a962e1b8cdb68fdd4dd3a7064827f8e38ece6c5
MiguelQuinones <42755523+MiguelQuinones@users.noreply.github.com>
2020-03-12 11:57:31 -0700

    Added potential switch statement for TransactionManagerWorker

--- TransactionManagerWorker.java
+++ TransactionManagerWorker.java
@@ -10,5 +10,20 @@ Socket client = null
 // TransactionManagerWorker handles openTransaction requests, write requests, read requests, and closeTransaction requests
 public class TransactionManagerWorker extends Thread
 {
-
+    // switch( /* switch on type of incoming request*/ )
+    // {
+    //     case OPEN_TRANSACTION:
+    //         // take incoming port of transaction
+    //         // open connection
+    //         break;
+    //     case READ:
+    //         // send back information to client without writing anything new
+    //         break;
+    //     case WRITE:
+    //         // write new information as requested in transaction
+    //         break;
+    //     case CLOSE_TRANSACTION:
+    //         // close connection
+    //         break;
+    // }
 }
