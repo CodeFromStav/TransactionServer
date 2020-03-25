a4d21a26629352e65c8a61156a1de3b5fc622108
Stavros <stav.triantis@gmail.com>
2020-03-25 13:16:08 -0700

    added TransactionClient run loop with threading.

--- TransactionClient.java
+++ TransactionClient.java
@@ -2,11 +2,42 @@
 // Transaction Client class
 public class TransactionClient extends Thread
 {
+  @Override
   public void run()
   {
     // Spawns off proxy threads
-    TServerProxy worker = new TServerProxy();
-    Thread proxyWorker = new Thread( worker );
-    proxyWorker.start();
+    // TServerProxy worker = new TServerProxy();
+    // Thread proxyWorker = new Thread( worker );
+    // proxyWorker.start();
+    int index;
+    for ( index = 0; index < numberTransactions; index++ )
+    {
+      new Thread()
+      {
+        @Override
+        public void run()
+        {
+          TransactionServerProxy transaction = new TransactionServerProxy( host, port );
+          int transID = transaction.openTransaction();
+          System.out.println( "transaction #" + transID + " started" );
+
+          int accountFrom = (int) Math.floor(Math.random() * numberAccounts );
+          int accountTo = (int) Math.floor(Math.random() * numberAccounts );
+          int amount = (int) Math.ceil(Math.random() * initialBalance );
+          int balance;
+          System.out.println( "\ttransaction #" + transID + ", $" + amount + " " + accountFrom + "->" + accountTo );
+
+          balance = transaction.read(accountFrom);
+          transaction.write( accountFrom, balance - amount );
+
+          balance = transaction.read(accountTo);
+          transaction.write( accountTo, balance + amount );
+
+          transaction.closeTransaction();
+
+          System.out.println("transaction #" + transID + " finished");
+        }
+      }.start();
+    }
   }
 }
