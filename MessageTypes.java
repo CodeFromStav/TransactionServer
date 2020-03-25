ae41b1859e833b65ec8ac9e0c0c9a899690bac11
MiguelQuinones <42755523+MiguelQuinones@users.noreply.github.com>
2020-03-25 13:39:58 -0700

    Added Message and MessageTypes classes

---
+++ MessageTypes.java
@@ -0,0 +1,10 @@
+
+// MessageTypes interface
+public interface MessageTypes
+{
+    // Enumerates the different types of messages
+    public static final int OPEN_TRANSACTION = 1;
+    public static final int CLOSE_TRANSACTION = 2;
+    public static final int READ = 3;
+    public static final int WRITE = 4;
+}
