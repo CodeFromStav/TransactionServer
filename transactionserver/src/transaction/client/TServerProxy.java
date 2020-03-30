4a73766f934fdaefe13d0a7347298bc3a53c6880
Stavros Triantis <38473035+CodeFromStav@users.noreply.github.com>
2020-03-29 20:42:33 -0700

    Add files via upload
    All classes and code in eclipse

---
+++ transactionserver/src/transaction/client/TServerProxy.java
@@ -0,0 +1,99 @@
+package transaction.client;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import transaction.comm.Message;
+import transaction.comm.MessageTypes;
+import java.net.Socket;
+
+// Transaction Server Proxy class
+//Transaction client hits proxy, then the proxy server manages workers.
+//represents transaction, which spins threads.
+public class TServerProxy implements MessageTypes
+{
+  String host = null;
+  int port;
+
+  private Socket dbConnection = null;
+  private ObjectOutputStream writeToNet = null;
+  private ObjectInputStream readFromNet = null;
+  private Integer transID = 0;
+
+  TServerProxy(String host, int port) throws Exception
+  {
+    this.host = host;
+    this.port = port;
+  }
+
+  public int openTransaction()
+  {
+    // create socket connection
+    try
+    {
+        // connect to the server loop
+        dbConnection = new Socket(host, port);
+        System.out.println("[TransactionServerProxy.open] Proxy connected!");
+        // assign the object input and output
+        writeToNet = new ObjectOutputStream(dbConnection.getOutputStream());
+        readFromNet = new ObjectInputStream(dbConnection.getInputStream());
+        transID++;
+        return transID;
+
+    }
+    catch (IOException e)
+    {
+      System.out.println("[TransactionServerProxy.open] Proxy error occurred!");
+      e.printStackTrace();
+    }
+ 
+  }
+
+  public void closeTransaction()
+  {
+    dbConnection.close
+    System.out.println("[TransactionServerProxy.close] the transaction has ended");
+
+  }
+
+  public int read(int accountNumber)
+  {
+    Message readMessage = new Message(READ_REQUEST, accountNumber);
+    Integer balance = null;
+
+    try
+    {
+      writeToNet.writeObject(readMessage);
+      balance = (Integer) readFromNet.readObject();
+    }
+    catch (Exception ex)
+    {
+      System.out.println("[TServerProxy.read] Error occured while trying to read " );
+      ex.printStackTrace();
+    }
+
+    return balance;
+
+  }
+
+  public int write(int accountNumber, int amount)
+  {
+    Message writeMessage = new Message( WRITE_REQUEST, accountNumber );
+    Integer balance = null;
+
+    try
+    {
+      writeToNet.writeObject(writeMessage);
+      balance (Integer) writeFromNet.readObject();
+      // TODO
+    }
+    catch (Exception ex)
+    {
+      System.out.println( "[TServerProxy.write] Error occured." );
+      ex.printStackTrace();
+    }
+
+    return balance;
+  }
+
+}
